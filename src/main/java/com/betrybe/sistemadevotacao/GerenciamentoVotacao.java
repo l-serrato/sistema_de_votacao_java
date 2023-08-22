a92bc380e083ba3f7aaad11df42ef3837a19dbf8
Luis <l-serrato@live.com>
2023-08-22 19:59:02 -0300

    req4?

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -2,8 +2,32 @@ package com.betrybe.sistemadevotacao;
 
 import java.util.ArrayList;
 
-public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
-  private ArrayList<String> cpfsComputados;
-  private ArrayList<PessoaCandidata> pessoasCandidatas;
-  private ArrayList<PessoaEleitora> pessoasEleitoras;
-}
+  /** Javadoc.
+   * Comment.
+   */
+  public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+
+    private ArrayList<PessoaCandidata> pessoasCandidatas;
+    private ArrayList<PessoaEleitora> pessoasEleitoras;
+    private ArrayList<String> cpfsComputados;
+
+    @Override
+    public void cadastrarPessoaCandidata(String nome, int numero) {
+
+    }
+
+    @Override
+    public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+    }
+
+    @Override
+    public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+    }
+
+    @Override
+    public void mostrarResultado() {
+
+    }
+  }
