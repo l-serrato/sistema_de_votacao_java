e80b7ea344b3735a2e222a46bcf841f5149e6b36
Luis <l-serrato@live.com>
2023-08-22 19:26:12 -0300

    req3?

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,22 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  /** Javadoc.
+   * Parametro.
+   * Param.
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    super.setNome(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
